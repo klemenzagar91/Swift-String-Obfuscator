a326ef17fe83918e11a47f8c249f84d72fd87f70
Klemen Zagar <klemen.zagar91@gmail.com>
2023-12-04 15:31:46 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -10,8 +10,9 @@ let package = Package(
         .executable(name: "swift_string_obfuscator", targets: ["swift_string_obfuscator"]),
     ],
     dependencies: [
-        .package(name: "SwiftSyntax", url: "https://github.com/apple/swift-syntax.git", .exact("0.50500.0")),
-        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.1"),
+        ..package(name: "SwiftSyntax", url: "https://github.com/apple/swift-syntax.git", .revision("release/5.5")),
+        .package(url: "https://github.com/SwiftDocOrg/SwiftSemantics.git", .upToNextMinor(from: "0.3.0")),
+        .package(url: "https://github.com/apple/swift-argument-parser.git", .upToNextMinor(from: "0.3.2")),
     ],
     targets: [
         .target(
